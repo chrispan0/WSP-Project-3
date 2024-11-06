3b2ab588e651d03c416860aceeb0ad7216a1444a
chrispan0 <github@chrispanetta.com>
2024-11-06 17:25:15 -0500

    added a count function and fetches tickets

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var crypto = require('crypto');
-var ticket = require('../model/ticket');
+var crypto = require("crypto");
+var ticket = require("../model/ticket");
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Home" });
 });
-router.get('/ticket', function(req, res, next) {
-  res.render('ticket', { title: 'Ticket Editor' });
+router.get("/ticket", function (req, res, next) {
+  res.render("ticket", { title: "Ticket Editor" });
 });
-router.get('/manage', function(req, res, next) {
-  res.render('manage', { title: 'Ticket Manager' });
+
+router.get("/manage", async (req, res, next) => {
+  const ticket_list = await ticket.find();
+  res.render("manage", {
+    title: "Manage Tickets",
+    ticket_list: ticket_list,
+  });
 });
-router.post('/create', async(req, res, next) => {
-  var {name,email,title,description,type,priority} = req.body;
-  new_ticket = new ticket({id:crypto.randomUUID(),name,email,title,description,type,priority});
+
+router.post("/create", async (req, res, next) => {
+  var { name, email, title, description, type, priority } = req.body;
+  new_ticket = new ticket({
+    id: crypto.randomUUID(),
+    name,
+    email,
+    title,
+    description,
+    type,
+    priority,
+  });
   await new_ticket.save();
-  res.redirect('/');
+  res.redirect("/");
 });
 
 module.exports = router;
