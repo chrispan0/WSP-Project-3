2551ce06a4a7a408e8009142040cd833453087bb
Jackson Barker <jackson@jacksonbarker.ca>
2024-11-06 16:08:03 -0500

    Post form to mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
+var ticket = require('../model/ticket');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
@@ -9,6 +11,12 @@ router.get('/ticket', function(req, res, next) {
 });
 router.get('/manage', function(req, res, next) {
   res.render('manage', { title: 'Ticket Manager' });
+});
+router.post('/create', async(req, res, next) => {
+  var {name,email,title,description,type,priority} = req.body;
+  new_ticket = new ticket({id:crypto.randomUUID(),name,email,title,description,type,priority});
+  await new_ticket.save();
+  res.redirect('/');
 });
 
 module.exports = router;
