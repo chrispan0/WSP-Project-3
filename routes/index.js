351a7fce4f8d44356f35210d79559a8ffb8369d3
Burhan7860 <muhammad.chattha2@ontariotechu.net>
2024-11-11 18:32:29 -0500

    Added comments to routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
+// Import required modules
 var express = require("express");
 var router = express.Router();
 var crypto = require("crypto");
 var Ticket = require("../model/ticket");
-
+// Route to render the home page
 router.get("/", function (req, res, next) {
   if (req.query.submitted == "true") {
     res.render("index", { title: "CJB Support", submitted: true });
@@ -12,7 +13,7 @@ router.get("/", function (req, res, next) {
     res.render("index", { title: "CJB Support" });
   }
 });
-
+// Route to render the ticket editor page
 router.get("/editor", async (req, res, next) => {
   try {
     let ticket = await Ticket.findById(req.query.id);
@@ -25,34 +26,41 @@ router.get("/editor", async (req, res, next) => {
     res.redirect("/editor");
   }
 });
-
+// Route to render the manage tickets page
 router.get("/manage", async (req, res, next) => {
+// Fetch all tickets from the database
   const ticket_list = await Ticket.find();
+// Check the 'edited' and 'deleted' query parameters to determine the render state
   if (req.query.edited == "true") {
+ // Render the manage page with an edited success indicator
     res.render("manage", {
       title: "Manage Tickets",
       ticket_list: ticket_list,
       edited: true,
     });
   } else if (req.query.edited == "false") {
+  // Render the manage page with an edited failure indicator
     res.render("manage", {
       title: "Manage Tickets",
       ticket_list: ticket_list,
       edited: false,
     });
   } else if (req.query.deleted == "true") {
+  // Render the manage page with a deleted success indicator
     res.render("manage", {
       title: "Manage Tickets",
       ticket_list: ticket_list,
       deleted: true,
     });
   } else if (req.query.deleted == "false") {
+  // Render the manage page with a deleted failure indicator
     res.render("manage", {
       title: "Manage Tickets",
       ticket_list: ticket_list,
       deleted: false,
     });
   } else {
+// Render the manage page without any edit or delete status
     res.render("manage", {
       title: "Manage Tickets",
       ticket_list: ticket_list,
