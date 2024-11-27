f8a11fcbbd389fe793b7741b29f3747f29e20971
Chris Panetta <cv.panetta@gmail.com>
2024-11-27 10:36:01 -0500

    added pages

--- model/ticket.js
+++ model/ticket.js
@@ -1,19 +1,7 @@
 // MVC --> Model , View , Controller (Routers)
-let mongoose = require('mongoose')
+let mongoose = require("mongoose");
 // create a model class
 let ticketModel = new mongoose.Schema({
-<<<<<<< Updated upstream
-    id:String,
-    name:String,
-    email:String,
-    title:String,
-    description:String,
-    type:String,
-    priority:Number
-}
-)
-module.exports = mongoose.model('ticket',ticketModel)
-=======
   name: String,
   user: String,
   title: String,
@@ -22,4 +10,3 @@ module.exports = mongoose.model('ticket',ticketModel)
   priority: Number,
 });
 module.exports = mongoose.model("ticket", ticketModel);
->>>>>>> Stashed changes
